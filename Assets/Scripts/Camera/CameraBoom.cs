85ff450b6287612429ab2bffb65dd4920a2cf30c
Xymer <sebastian.unland@futuregames.nu>
2020-03-11 16:17:32 +0100

    Optimized the camera further

--- Assets/Scripts/Camera/CameraBoom.cs
+++ Assets/Scripts/Camera/CameraBoom.cs
@@ -13,38 +13,33 @@ public class CameraBoom : MonoBehaviour
     float cameraBoomLength = 10.0f;
     [SerializeField] float maxCameraBoomLength = 10.0f;
     [SerializeField] float minCameraBoomLength = 2.0f;
-    [SerializeField, Range(0f,1f)] float boomSmoothening = 0.75f;
+    [SerializeField, Range(0f, 1f)] float boomSmoothening = 0.75f;
 
     [SerializeField] float lerpMultiplier = 50.0f;
-    [SerializeField, Range(0f,1f)] float cameraReturnSmoothening = 0.05f;
+    [SerializeField, Range(0f, 1f)] float cameraReturnSmoothening = 0.05f;
     [SerializeField, Range(0f, 1f)] float movementLerpSmoothening = 0.5f;
 
     [SerializeField] float mouseAxisYMax = 45.0f;
-    [SerializeField] float mouseAxisYMin = -10.0f;
-
+    [SerializeField] float mouseAxisYMin = -5.0f;
+    [SerializeField, Tooltip("Minimum angle for target while turning")]float minTurnRotation = -10f;
+    [SerializeField, Tooltip("Maximum angle for target while turning")]float maxTurnRotation = 10f;
     float mouseAxisYValue = 0f;
     float mouseAxisXValue = 0f;
+    float freeCameraAxisXValue = 0;
 
     float targetBoomLength = 10f;
     float cameraSphereRadius = 1f;
-    float distanceToLockCamera = 1.0f;
+    float distanceToLockCamera = 5f;
     bool isReturning = false;
     bool isOverridingLength = false;
 
-    [SerializeField] Vector3 cameraBoomOffset = Vector3.zero;
-    [SerializeField] Vector3 cameraOffset = Vector3.zero;
+    [SerializeField, Tooltip("Offset from camera boom transform")] Vector3 cameraBoomOffset = Vector3.zero;
+    [SerializeField, Tooltip("Offset for camera")] Vector3 cameraOffset = Vector3.zero;
     Vector3 mouseOffset = Vector3.zero;
     Vector3 linetraceHitpoint;
     [Header("Mouse Settings"), SerializeField]
     float scrollWheelMultiplier = 10.0f;
-
     [SerializeField] float mouseMultiplier = 10.0f;
-    private float scrollWheelInput = 0.0f;
-
-    float freeCameraAxisXValue = 0;
-    float freeCameraAxisYValue = 0;
-
-
 
     Vector3 endPosition;
     Camera camera = default;
@@ -61,41 +56,45 @@ public class CameraBoom : MonoBehaviour
 
     // Update is called once per frame
     void LateUpdate()
-    {     
+    {
         if (LinecastFromTargetToCamera(ref linetraceHitpoint))
         {
-        OverrideCameraBoomLength(Vector3.Distance(endPosition, linetraceHitpoint));
+            OverrideCameraBoomLength(Vector3.Distance(endPosition, linetraceHitpoint));
         }
-
         else
         {
-        ReturnToTargetBoomLength();
-        }
-        
+            ReturnToTargetBoomLength();
+        }       
+
         if (GetScrollInput() != 0)
         {
             SetCameraBoomLength();
         }
+
+        UpdateMouseValues();
 
         if (!GetRightMouseInput())
         {
             UpdateEndPosition(cameraTarget);
             SetCameraPosition(cameraTarget);
             RotateCameraBoom(cameraTarget);
-            LerpMouseAxisBackToZero();        
+            LerpMouseAxisBackToZero();
         }
 
         if (GetRightMouseInput())
-        {
+        { 
             UpdateEndPosition(cameraTarget);
             SetFreeCameraPosition(cameraTarget);
-        }
+        }      
     }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(endPosition, 1.0f);
+        Gizmos.DrawWireSphere(endPosition, cameraSphereRadius);
+        if (camera != null)
+        {
         SetupCameraSphere(camera.transform.position, cameraSphereRadius);
+        }
     }
 
     void SetupCameraSphere(Vector3 cameraPosition, float radius)
@@ -105,7 +104,7 @@ public class CameraBoom : MonoBehaviour
     }
     void UpdateEndPosition(GameObject target)
     {
-        endPosition = Vector3.Lerp(endPosition,target.transform.position + cameraBoomOffset + mouseOffset,Time.deltaTime * lerpMultiplier * movementLerpSmoothening);
+        endPosition = Vector3.Lerp(endPosition, target.transform.position + cameraBoomOffset + mouseOffset, Time.deltaTime * lerpMultiplier * movementLerpSmoothening);
     }
     void SetCameraPosition(GameObject target)
     {
@@ -114,7 +113,9 @@ public class CameraBoom : MonoBehaviour
     }
     void RotateCameraBoom(GameObject target)
     {
-        if (Vector3.Distance(transform.rotation.eulerAngles, target.transform.rotation.eulerAngles) < distanceToLockCamera) // Decides if it should lock the camera behind the camera target
+        float lockValue = target.transform.rotation.eulerAngles.y - transform.rotation.eulerAngles.y;
+        Debug.Log(Mathf.Abs(lockValue));
+        if (Mathf.Abs(lockValue) < distanceToLockCamera) // Decides if it should lock the camera behind the camera target
         {
             isReturning = false;
         }
@@ -123,22 +124,23 @@ public class CameraBoom : MonoBehaviour
             mouseAxisYValue -= Input.GetAxis(mouseAxisY);
             mouseAxisXValue += Input.GetAxis(mouseAxisX);
             mouseAxisYValue = Mathf.Clamp(mouseAxisYValue, mouseAxisYMin, mouseAxisYMax);
-            Quaternion rotation =Quaternion.Euler(Mathf.Clamp(mouseAxisYValue + target.transform.rotation.eulerAngles.x,mouseAxisYMin,mouseAxisYMax), target.transform.rotation.eulerAngles.y + -Mathf.Clamp(mouseAxisXValue,-10f,10f), 0f);         
-            transform.rotation = rotation;      
+            Quaternion rotation = Quaternion.Euler(Mathf.Clamp(mouseAxisYValue + target.transform.rotation.eulerAngles.x, mouseAxisYMin, mouseAxisYMax),
+                target.transform.rotation.eulerAngles.y + -Mathf.Clamp(mouseAxisXValue, minTurnRotation, maxTurnRotation), 0f);
+            transform.rotation = rotation;
         }
         else if (isReturning)
-        {            
-            transform.rotation = Quaternion.Lerp(transform.rotation, target.transform.rotation, Time.deltaTime * lerpMultiplier * cameraReturnSmoothening);
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(target.transform.rotation.eulerAngles.x + mouseAxisYValue, target.transform.rotation.eulerAngles.y,
+                target.transform.rotation.eulerAngles.z), Time.deltaTime * lerpMultiplier * cameraReturnSmoothening);
         }
         camera.transform.LookAt(endPosition);
     }
     void SetFreeCameraPosition(GameObject target)
     {
+        
         SetCameraPosition(target);
-        Vector3 mouseMovement = new Vector3(freeCameraAxisXValue, Mathf.Clamp(freeCameraAxisYValue, mouseAxisYMin, mouseAxisYMax), 0);
+        Vector3 mouseMovement = new Vector3(freeCameraAxisXValue, Mathf.Clamp(mouseAxisYValue, mouseAxisYMin, mouseAxisYMax), 0);
         freeCameraAxisXValue += Input.GetAxis(mouseAxisX) * mouseMultiplier;
-        freeCameraAxisYValue -= Input.GetAxis(mouseAxisY) * mouseMultiplier;
-        freeCameraAxisYValue = Mathf.Clamp(freeCameraAxisYValue, mouseAxisYMin, mouseAxisYMax);
 
         Quaternion rotation = Quaternion.Euler(mouseMovement.y + target.transform.rotation.eulerAngles.x,mouseMovement.x + target.transform.rotation.eulerAngles.y, 0.0f);
         camera.transform.LookAt(endPosition);
@@ -154,11 +156,11 @@ public class CameraBoom : MonoBehaviour
     }
     void OverrideCameraBoomLength(float length)
     {
-         if (length < targetBoomLength)
+        if (length < targetBoomLength)
         {
             cameraBoomLength = Mathf.Lerp(cameraBoomLength, length, lerpMultiplier * Time.deltaTime * boomSmoothening);
             isOverridingLength = true;
-        }       
+        }
     }
     void ReturnToTargetBoomLength()
     {
@@ -176,18 +178,21 @@ public class CameraBoom : MonoBehaviour
     {
         return Input.GetAxis(scrollWheel) * Mathf.Abs(Input.GetAxis(scrollWheel));
     }
-    
+    void UpdateMouseValues()
+    {
+        mouseAxisYValue -= Input.GetAxis(mouseAxisY);
+        mouseAxisXValue += Input.GetAxis(mouseAxisX);
+        mouseAxisYValue = Mathf.Clamp(mouseAxisYValue, mouseAxisYMin, mouseAxisYMax);
+    }
     void LerpMouseAxisBackToZero()
     {
         //Regularcam axis
         if (Input.GetAxis(mouseAxisX) == 0 || Input.GetAxis(mouseAxisY) == 0)
         {
-        mouseAxisXValue = Mathf.Lerp(mouseAxisXValue, 0, Time.deltaTime * lerpMultiplier * cameraReturnSmoothening);
-        //mouseAxisYValue = Mathf.Lerp(mouseAxisYValue, 0, Time.deltaTime * lerpMultiplier * cameraReturnSmoothening);
+            mouseAxisXValue = Mathf.Lerp(mouseAxisXValue, 0, Time.deltaTime * lerpMultiplier * cameraReturnSmoothening);
         }
         //Freecam axis
-        freeCameraAxisXValue = Mathf.Lerp(freeCameraAxisXValue, 0, Time.deltaTime * lerpMultiplier * cameraReturnSmoothening);
-        freeCameraAxisYValue = Mathf.Lerp(freeCameraAxisYValue, 0, Time.deltaTime * lerpMultiplier * cameraReturnSmoothening);
+        freeCameraAxisXValue = Mathf.Lerp(freeCameraAxisXValue, 0, Time.deltaTime * lerpMultiplier * cameraReturnSmoothening);    
     }
     bool GetRightMouseInput()
     {
@@ -199,11 +204,20 @@ public class CameraBoom : MonoBehaviour
         RaycastHit hitInfo;
         bool hasHitObject = Physics.Linecast(endPosition, camera.transform.position - camera.transform.forward * 2f, out hitInfo);
         Debug.DrawLine(endPosition, camera.transform.position - camera.transform.forward * 2f);
-        
+
         if (hasHitObject)
         {
             hitPoint = hitInfo.point;
         }
-            return hasHitObject;
+        return hasHitObject;
+    }
+
+    /// <summary>
+    /// Switch target to follow
+    /// </summary>
+    /// <param name="target"></param>
+    void ChangeCameraTarget(GameObject target)
+    {
+        cameraTarget = target;
     }
 }
